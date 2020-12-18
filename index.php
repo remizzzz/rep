ea7fd4056473ab85acc09a7050c4fdafff1cad02
remizzzz <remizov.di@mail.ru>
2020-12-18 16:03:06 +0300

    2 lab

--- index.php
+++ index.php
@@ -1,35 +1,42 @@
 <?php
 
-class A
-{
+class A {
+    function solve($b, $c): array {
+        if ($b === 0) {
+            throw new InvalidArgumentException("Equation doesn't exist");
+        }
 
+        $this->X = -($b / $c);
+        return [$this->X];
+    }
+    protected $X;
 }
 
 
-class B extends A
-{
-    protected $a;
-    public function __construct($a)
-    {
-        $this->a = $a;
+class B extends A {
+    function solve($a, $b, $c): array {
+        if ($a === 0) {
+            return parent::solve($b, $c);
+        }
+
+        $discriminant = pow($b, 2) - 4 * $a * $c;
+
+        if ($discriminant < 0) {
+            throw new InvalidArgumentException("Invalid quadratic eq: discriminant is less than 0");
+        }
+
+        $this->X = [(-$b + sqrt($discriminant)) / 2 / $c,
+                    ($b + sqrt($discriminant)) / 2 / $c];
+
+        return $this->X;
     }
 }
 
-class C extends B
-{
-    protected $a, $b;
-    public function __construct($a, $b)
-    {
-        $this->a = $a;
-        $this->b = $b;
-        parent::__construct($a);
-    }
+$eq = new B();
+$solutions = $eq->solve(0,2, 4);
+
+foreach ($solutions as $s) {
+    echo $s . '  ';
 }
 
-$a = new A();
-$b1 = new B($a);
-$b2 = new B($a);
-$b3 = new B($b1);
-$c = new C($b2, $b3);
-
-?> 
+?>
